a12c722ebfb23d50f39831146e5ec6c8e74178cb
superpoupette <excaaalibur@gmail.com>
2026-07-07 16:41:49 +0200

    Add functions to load and prepare Babelio data

--- scripts/importation_babelio.py
+++ scripts/importation_babelio.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def load_babelio(filepath):
+    livres = pd.read_csv(filepath)
+    return livres
+
+def prepare_babelio(filepath):
+    livres = load_babelio(filepath)
+    return livres
