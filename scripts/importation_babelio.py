09a77ab5ea69ce95a71a8a7586ee3ec1dac11560
superpoupette <excaaalibur@gmail.com>
2026-07-07 17:01:53 +0200

    Add encoding parameter to CSV loading
    Updated load_babelio function to specify encoding.

--- scripts/importation_babelio.py
+++ scripts/importation_babelio.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 def load_babelio(filepath):
-    livres = pd.read_csv(filepath)
+    livres = pd.read_csv(
+        filepath,
+        encoding="latin-1"
+    )
     return livres
 
 def prepare_babelio(filepath):
