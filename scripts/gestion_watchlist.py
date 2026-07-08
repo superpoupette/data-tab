6e9f726e19a1360c011241a1fc9aa1f346526b0b
superpoupette <excaaalibur@gmail.com>
2026-07-08 14:55:31 +0200

    Update gestion_watchlist.py

--- scripts/gestion_watchlist.py
+++ scripts/gestion_watchlist.py
@@ -5,13 +5,15 @@ def derniers_visionnages(movies, series):
     watched_movies = (
         movies[movies["status"] == "watched"]
         .rename(columns={"watched_at": "date"})
-        [["title", "type", "status", "date"]]
+        .assign(episode=pd.NA)
+        [["title", "type", "status", "episode", "date"]]
     )
 
     watched_series = (
         series[series["status"] == "up_to_date"]
-        .rename(columns={"last_watch": "date"})
-        [["title", "type", "status", "date"]]
+        .rename(columns={"last_watch": "date",
+                         "last_episode": "episode"})
+        [["title", "type", "status", "episode", "date"]]
     )
 
     derniers_visionnages = pd.concat(
