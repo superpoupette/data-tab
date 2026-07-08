69ef7d485155944005ea862121bd821a68f9cc6d
superpoupette <excaaalibur@gmail.com>
2026-07-08 09:53:10 +0200

    maj tv

--- pages/3_tv.py
+++ pages/3_tv.py
@@ -1,1 +1,13 @@
 import streamlit as st
+
+
+from scripts.importation_tvtime import tab_tv
+
+tvtime = tab_tv()
+
+st.title("Données TV Time")
+
+st.dataframe(
+    tvtime,
+    use_container_width=True
+)
--- scripts/importation_tvtime.py
+++ scripts/importation_tvtime.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+def load_tv(filepath):
+    tv = pd.read_csv(filepath)
+    return tv
+
+
+def tab_tv():
+    tv=load_tv("data/tvtime.csv")
+    return tv
