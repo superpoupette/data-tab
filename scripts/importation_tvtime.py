ae54d7c6d0bf17027cce5b8fa9401a5cd0aece05
superpoupette <excaaalibur@gmail.com>
2026-07-08 10:08:50 +0200

    Update importation_tvtime.py

--- scripts/importation_tvtime.py
+++ scripts/importation_tvtime.py
@@ -4,9 +4,19 @@ def load_tv(filepath):
     tv = pd.read_csv(filepath)
     return tv
 
+def clean_movies(movies):
+    movies["type"] = "movie"
+    return movies
+
+def clean_series(series):
+    series["type"] = "series"
+    return series
+
 
 def tab_tv():
     movies=load_tv("data/tvtime-movies-2026-07-07.csv")
     series=load_tv("data/tvtime-series-2026-07-07.csv")
     series_episodes=load_tv("data/tvtime-series-episodes-2026-07-07.csv")
+    movies = clean_movies(movies)
+    series = clean_series(series)
     return movies, series, series_episodes
