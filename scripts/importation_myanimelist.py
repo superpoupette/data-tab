1e0eda29521260e94aa14267df8691497ddee8a3
superpoupette <excaaalibur@gmail.com>
2026-07-08 15:41:59 +0200

    Update importation_myanimelist.py

--- scripts/importation_myanimelist.py
+++ scripts/importation_myanimelist.py
@@ -16,24 +16,22 @@ def load_myanimelist(filepath):
             "episodes": int(anime.findtext("series_episodes") or 0),
             "progress": int(anime.findtext("my_watched_episodes") or 0),
             "score": int(anime.findtext("my_score") or 0),
-            "start_date": anime.findtext("my_start_date"),
-            "finish_date": anime.findtext("my_finish_date"),
         })
 
-    animes = pd.DataFrame(animes)
+    return pd.DataFrame(animes)
 
-    animes["start_date"] = pd.to_datetime(
-        animes["start_date"],
-        errors="coerce"
-    )
-
-    animes["finish_date"] = pd.to_datetime(
-        animes["finish_date"],
-        errors="coerce"
+def clean_animes(animes):
+    animes["status"] = animes["status"].replace(
+        {
+            "Completed": "watched",
+            "Watching": "watching",
+            "Plan to Watch": "to_watch"
+        }
     )
 
     return animes
 
-
 def tab_myanimelist():
-    return load_myanimelist("data/animelist.xml")
+    animes = load_myanimelist("data/animelist.xml")
+    animes = clean_animes(animes)
+    return animes
