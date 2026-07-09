e7e65e303ebfaacd769b82f2da27a68a51b66b1f
superpoupette <excaaalibur@gmail.com>
2026-07-09 14:57:19 +0200

    test

--- pages/today.py
+++ pages/today.py
@@ -23,7 +23,7 @@ best_moment = st.text_input("Meilleur moment du jour :")
 col1, col2 = st.columns([1, 5])
 
 with col1:
-    st.write("Travail")
+    st.write("Travail :")
     people_work = st.checkbox(
         " ",
         label_visibility="collapsed"
@@ -35,6 +35,7 @@ with col2:
         placeholder="Ex : Alice, Marc, Julie"
     )
 
+sommeil = st.text_input("Sommeil :")
 
 # Création du dataframe
 df = create_today_dataframe()
@@ -48,6 +49,7 @@ if st.button("💾 Sauvegarder"):
         best_moment,
         people_seen,
         int(people_work)
+        int(sommeil),
     )
 
 
--- scripts/data_entry/dataframe.py
+++ scripts/data_entry/dataframe.py
@@ -6,17 +6,19 @@ def create_today_dataframe():
         "date",
         "best_moment",
         "people_seen",
-        "people_work"
+        "people_work",
+        "sommeil"
     ])
 
 
-def add_today_entry(df, today_date, best_moment, people_seen, people_work):
+def add_today_entry(df, today_date, best_moment, people_seen, people_work, sommeil):
 
     new_row = pd.DataFrame([{
         "date": today_date,
         "best_moment": best_moment,
         "people_seen": people_seen,
-        "people_work": people_work
+        "people_work": people_work,
+        "sommeil": sommeil
     }])
 
     df = pd.concat([df, new_row], ignore_index=True)
