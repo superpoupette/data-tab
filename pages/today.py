16b093641f7f9bccfc6f31bb54d50362543b45d1
superpoupette <excaaalibur@gmail.com>
2026-07-09 14:14:14 +0200

    Update today.py

--- pages/today.py
+++ pages/today.py
@@ -5,9 +5,9 @@ st.set_page_config(page_title="today")
 st.title("Données du jour")
 
 
-st.subheader("Moment du jour")
+st.subheader("Mood")
 
-best_moment = st.text_input("Nom")
+best_moment = st.text_input("Meilleur moment du jour :")
 
 
 st.write(best_moment)
