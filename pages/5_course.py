b688a2182a8fa8a954f5f4e267a0dcf224cdb228
superpoupette <excaaalibur@gmail.com>
2026-07-16 16:35:06 +0200

    Update 5_course.py

--- pages/5_course.py
+++ pages/5_course.py
@@ -2,14 +2,16 @@ import streamlit as st
 
 from scripts.importation_strava import charger_donnees_strava
 
-st.write(df.columns[:5])
-st.write(df["Nom de l'activité"].head())
+
 
 st.set_page_config(
     page_title="Course",
     page_icon="🏃",
     layout="wide"
 )
+
+st.write(df.columns[:5])
+st.write(df["Nom de l'activité"].head())
 
 st.title("🏃 Dashboard Course")
 
