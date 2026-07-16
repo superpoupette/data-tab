91824319530f271a82059d6ed962275ca75d0c3c
superpoupette <excaaalibur@gmail.com>
2026-07-16 16:33:04 +0200

    f

--- scripts/importation_strava.py
+++ scripts/importation_strava.py
@@ -1,26 +1,24 @@
 import pandas as pd
 
 
-def reparer_texte(texte):
-    if not isinstance(texte, str):
-        return texte
-
-    marqueurs_corruption = ["Ã", "Â", "ð", "�"]
-
-    if any(m in texte for m in marqueurs_corruption):
+def reparer_texte(x):
+    if isinstance(x, str):
         try:
-            return texte.encode("latin1").decode("utf-8")
-        except Exception:
-            return texte
+            return x.encode("latin1").decode("utf-8")
+        except:
+            return x
+    return x
 
-    return texte
+
+df = df.applymap(reparer_texte)
 
 def charger_donnees_strava():
+
     df = pd.read_csv(
         "data/strava_activities.csv",
-        encoding="latin1"
+        encoding="utf-8-sig"
     )
 
-    df = df.map(reparer_texte)
+    df = df.applymap(reparer_texte)
 
     return df
