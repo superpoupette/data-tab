f79fd56fe32692379a5d0fe5e17f9f7f04c9903a
superpoupette <excaaalibur@gmail.com>
2026-07-16 16:26:56 +0200

    g

--- scripts/importation_strava.py
+++ scripts/importation_strava.py
@@ -1,14 +1,21 @@
 import pandas as pd
 
 
-def charger_donnees_strava():
-    """
-    Charge le fichier CSV Strava et retourne un DataFrame.
-    """
+def reparer_texte(texte):
+    if isinstance(texte, str):
+        try:
+            return texte.encode("latin1").decode("utf-8")
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            return texte
+    return texte
 
+
+def charger_donnees_strava():
     df = pd.read_csv(
         "data/strava_activities.csv",
         encoding="latin1"
     )
+
+    df = df.map(reparer_texte)
 
     return df
