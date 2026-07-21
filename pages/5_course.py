52a3be33db65ffb4ad2b5a110f247794951f3f12
superpoupette <excaaalibur@gmail.com>
2026-07-21 11:16:58 +0200

    Update 5_course.py

--- pages/5_course.py
+++ pages/5_course.py
@@ -300,11 +300,11 @@ for col, (_, ligne) in zip(
         f"{ligne['Allure moyenne']}"
     )
 
-st.markdown("<br><br>", unsafe_allow_html=True)
+
 
 st.markdown(
     """
-    <hr style="border:1px solid #e0e0e0;">
+    <hr style="border:0.5px solid #e0e0e0;">
     """,
     unsafe_allow_html=True
 )
