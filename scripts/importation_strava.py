524d0aca31570a1116c34f1c897733597f71183b
superpoupette <excaaalibur@gmail.com>
2026-07-21 09:28:59 +0200

    Update importation_strava.py

--- scripts/importation_strava.py
+++ scripts/importation_strava.py
@@ -1,6 +1,5 @@
 import pandas as pd
-
-
+import streamlit as st
 
 
 def charger_donnees_strava():
@@ -9,6 +8,14 @@ def charger_donnees_strava():
         "data/strava_activities.csv",
         encoding="utf-8",
         low_memory=False
+    )
+
+    # Conversion de la date
+    df["Date de l'activité"] = pd.to_datetime(
+        df["Date de l'activité"],
+        format="mixed",
+        dayfirst=True,
+        errors="coerce"
     )
 
     return df
