95986d2dd15cf55e85417d54d7e9ae7fedb1b70d
superpoupette <excaaalibur@gmail.com>
2026-07-23 15:32:18 +0200

    Update objectifs.py

--- scripts/objectifs.py
+++ scripts/objectifs.py
@@ -7,13 +7,12 @@ def pompes_2026():
 
     data = prepare_2026()
 
-    # Vérification présence colonne
     if "Pompes" not in data.columns:
         return 0
 
     pompes = (
         pd.to_numeric(
-            data["Pompes"],
+            data["Pompes"].iloc[1:],  # ignore la première ligne
             errors="coerce"
         )
         .fillna(0)
