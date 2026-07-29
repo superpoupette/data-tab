7e27c080c17d76748cc04ed4d6fbd196ad1f0b5e
superpoupette <excaaalibur@gmail.com>
2026-07-29 16:41:43 +0200

    Update data_loader.py

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -8,6 +8,7 @@ from scripts.importation_2025 import clean_csv as clean_2025
 from scripts.importation_2026 import clean_2026
 
 from scripts.importation_hevy import prepare_data
+from scripts.importation_babelio import prepare_babelio
 
 
 FILES_DRIVE = {
@@ -27,7 +28,12 @@ FILES_DRIVE = {
     2026: {
         "id": "1PVyEQ02T-TEfofWoJAtzorob6AU8tImU",
         "type": "excel"
-    },
+    }
+
+}
+
+
+OTHER_FILES = {
 
     "babelio": {
         "id": "1umai5aXgS22YKV3Mgk2tbvklrg9sFt_T",
@@ -52,10 +58,12 @@ HEVY_FILES = {
 
 }
 
+
 STRAVA_FILE = {
     "id": "1ns7SCQfEc4YsycnzjH2lUk7Q8gOBnNhc",
     "separator": ","
 }
+
 
 
 def load_year(year):
@@ -65,7 +73,9 @@ def load_year(year):
             f"Aucune donnée disponible pour {year}"
         )
 
+
     config = FILES_DRIVE[year]
+
 
     if config["type"] == "csv":
 
@@ -74,6 +84,7 @@ def load_year(year):
             separator=config["separator"]
         )
 
+
     elif config["type"] == "excel":
 
         data = load_excel_from_drive(
@@ -81,22 +92,33 @@ def load_year(year):
             sheet_name="DATA"
         )
 
+
     else:
 
         raise ValueError(
             f"Type de fichier inconnu : {config['type']}"
         )
 
+
+
     if year == 2024:
+
         data = clean_2024(data)
 
+
     elif year == 2025:
+
         data = clean_2025(data)
 
+
     elif year == 2026:
+
         data = clean_2026(data)
 
+
     return data
+
+
 
 
 def load_hevy():
@@ -106,15 +128,20 @@ def load_hevy():
         separator=HEVY_FILES["workouts"]["separator"]
     )
 
+
     exercices = load_csv_from_drive(
         HEVY_FILES["exercises"]["id"],
         separator=HEVY_FILES["exercises"]["separator"]
     )
 
+
     return prepare_data(
         workouts,
         exercices
     )
+
+
+
 
 def load_danse_2026():
 
@@ -123,14 +150,21 @@ def load_danse_2026():
         sheet_name="DANSE"
     )
 
-    return clean_danses_2026(danse)
+
+    return danse
+
+
 
 
 def load_babelio():
 
     data = load_csv_from_drive(
-        FILES_DRIVE["babelio"]["id"],
-        separator=FILES_DRIVE["babelio"]["separator"]
+        OTHER_FILES["babelio"]["id"],
+        separator=OTHER_FILES["babelio"]["separator"]
     )
+
+
+    data = prepare_babelio(data)
+
 
     return data
