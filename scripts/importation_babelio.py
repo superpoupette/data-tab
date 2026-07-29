cdfbe12b386587828eee55e0814c2ff991ef0f88
superpoupette <excaaalibur@gmail.com>
2026-07-29 16:48:27 +0200

    Update importation_babelio.py

--- scripts/importation_babelio.py
+++ scripts/importation_babelio.py
@@ -1,14 +1,5 @@
 import pandas as pd
 
-def load_babelio(filepath):
-    livres = pd.read_csv(
-        filepath,
-        sep=";",
-        encoding="cp1252",
-        quotechar='"'
-    )
-    return livres
+def prepare_babelio(data):
 
-def prepare_babelio(filepath):
-    livres = load_babelio(filepath)
-    return livres
+    return data
