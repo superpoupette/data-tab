be4e340940e36f9312c1f3c2ca3020febb11665f
superpoupette <excaaalibur@gmail.com>
2026-07-29 14:04:47 +0200

    Update importation_2024.py

--- scripts/importation_2024.py
+++ scripts/importation_2024.py
@@ -8,31 +8,46 @@ def load_csv(filepath):
 
 def clean_csv(data2024):
 
+    # Conversion date
     data2024["Date"] = pd.to_datetime(
         data2024["Date"],
         format="%d/%m/%y"
     )
 
-    data2024 = data2024.rename(
-    columns={
+
+    # Renommage des colonnes chorées
+    rename_columns = {
         "Chorée": "Choree1_morceau",
-        "Chorée.1": "Choree2_morceau",
-        "Chorée.2": "Choree3_morceau",
-        "Chorée.3": "Choree4_morceau",
-        "Chorée.4": "Choree5_morceau",
+        "Chorée_1": "Choree2_morceau",
+        "Chorée_3": "Choree3_morceau",
+        "Chorée_8": "Choree4_morceau",
+        "Chorée_10": "Choree5_morceau",
 
         "Temps": "Choree1_duree",
-        "Temps.1": "Choree2_duree",
-        "Temps.2": "Choree3_duree",
-        "Temps.5": "Choree4_duree",
-        "Temps.6": "Choree5_duree",
+        "Temps_2": "Choree2_duree",
+        "Temps_4": "Choree3_duree",
+        "Temps_9": "Choree4_duree",
+        "Temps_11": "Choree5_duree",
+    }
+
+
+    # Renomme uniquement les colonnes présentes
+    data2024 = data2024.rename(
+        columns={
+            old: new
+            for old, new in rename_columns.items()
+            if old in data2024.columns
         }
     )
+
 
     return data2024
 
 
 def prepare_2024(filepath):
+
     data2024 = load_csv(filepath)
+
     data2024 = clean_csv(data2024)
+
     return data2024
